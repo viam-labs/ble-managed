7b0d3b7f64f56e18fd6a190d5878661204c75ae1
Benji Rewis <benji.rewis@viam.com>
2024-07-03 17:26:29 +0000

    gatt

--- rust_viam_server/src/main.rs
+++ rust_viam_server/src/main.rs
@@ -1,21 +1,106 @@
-//! Connects to l2cap server and sends and receives test data.
+//! Scans for BLE device with Viam service UUID and PSM char, and opens L2CAP
+//! socket over that advertised PSM.
 
 use bluer::{
     l2cap::{SocketAddr, Stream},
-    Address, AddressType,
+    AdapterEvent, Address, AddressType, Device,
 };
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use futures::{pin_mut, StreamExt};
+use std::time::Duration;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    time::sleep,
+};
+use uuid::uuid;
 
-const PSM: u16 = 192;
+/// Service UUID for GATT example.
+const VIAM_SERVICE_UUID: uuid::Uuid = uuid!("79cf4eca-116a-4ded-8426-fb83e53bc1d7");
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> bluer::Result<()> {
+/// Characteristic UUID for GATT example.
+const PSM_CHARACTERISTIC_UUID: uuid::Uuid = uuid!("ab76ead2-b6e6-4f12-a053-61cd0eed19f9");
+
+async fn find_address_and_psm() -> bluer::Result<(Device, u16)> {
     let session = bluer::Session::new().await?;
     let adapter = session.default_adapter().await?;
     adapter.set_powered(true).await?;
 
-    let target_addr: Address = "FILLIN".parse().expect("invalid address");
-    let target_sa = SocketAddr::new(target_addr, AddressType::LeRandom, PSM);
+    println!(
+        "Discovering on Bluetooth adapter {} with address {}\n",
+        adapter.name(),
+        adapter.address().await?
+    );
+    let discover = adapter.discover_devices().await?;
+
+    pin_mut!(discover);
+    while let Some(evt) = discover.next().await {
+        match evt {
+            AdapterEvent::DeviceAdded(addr) => {
+                let device = adapter.device(addr)?;
+                let addr = device.address();
+                let uuids = device.uuids().await?.unwrap_or_default();
+
+                if uuids.contains(&VIAM_SERVICE_UUID) {
+                    println!("    Device {addr} provides our service!");
+
+                    sleep(Duration::from_secs(2)).await;
+                    if !device.is_connected().await? {
+                        println!("    Connecting...");
+                        let mut retries = 2;
+                        loop {
+                            match device.connect().await {
+                                Ok(()) => break,
+                                Err(err) if retries > 0 => {
+                                    println!("    Connect error: {}", &err);
+                                    retries -= 1;
+                                }
+                                Err(err) => return Err(err),
+                            }
+                        }
+                        println!("    Connected");
+                    } else {
+                        println!("    Already connected");
+                    }
+
+                    println!("    Enumerating services...");
+                    for service in device.services().await? {
+                        let uuid = service.uuid().await?;
+                        println!("    Service UUID: {}", &uuid);
+                        if uuid == VIAM_SERVICE_UUID {
+                            println!("    Found our service!");
+                            for char in service.characteristics().await? {
+                                let uuid = char.uuid().await?;
+                                println!("    Characteristic UUID: {}", &uuid);
+                                if uuid == PSM_CHARACTERISTIC_UUID {
+                                    println!("    Found our characteristic!");
+                                    if char.flags().await?.read {
+                                        println!("    Reading characteristic value");
+                                        let value = char.read().await?;
+                                        println!("    Read value: {:x?}", &value);
+                                        let str_psm = String::from_utf8_lossy(&value);
+                                        let psm = str_psm
+                                            .parse::<u16>()
+                                            .expect("Failed to convert psm to u16");
+                                        return Ok((device, psm));
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    println!("    Not found!");
+                }
+            }
+            _ => (),
+        }
+    }
+    Err(bluer::Error {
+        kind: bluer::ErrorKind::Failed,
+        message: "failed".to_string(),
+    })
+}
+
+async fn run_l2cap(target_addr: Address, psm: u16) -> bluer::Result<()> {
+    let target_sa = SocketAddr::new(target_addr, AddressType::LeRandom, psm);
 
     println!("Connecting to {:?}", &target_sa);
     let mut stream = Stream::connect(target_sa).await.expect("connection failed");
@@ -43,5 +128,24 @@ async fn main() -> bluer::Result<()> {
 
     println!("Done");
     stream.shutdown().await.expect("shutdown failed");
+    Ok(())
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> bluer::Result<()> {
+    let (device, psm) = find_address_and_psm()
+        .await
+        .expect("finding address and psm failed");
+
+    run_l2cap(device.address(), psm)
+        .await
+        .expect("opening l2cap socket failed");
+
+    match device.disconnect().await {
+        Ok(()) => println!("    Device disconnected"),
+        Err(err) => println!("    Device disconnection failed: {}", &err),
+    }
+    println!();
+
     Ok(())
 }
