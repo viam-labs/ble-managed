6c90f73878f53b7dd48f1896826c72a329d0af08
Benji Rewis <benji.rewis@viam.com>
2024-07-10 15:30:40 +0000

    remove golang

--- golang_viam_server/etc/commands.sh
+++
@@ -1,2 +0,0 @@
-echo "file net/bluetooth/l2cap_core.c +pfl" > /sys/kernel/debug/dynamic_debug/control
-echo "file net/bluetooth/l2cap_sock.c +pfl" > /sys/kernel/debug/dynamic_debug/control
--- golang_viam_server/l2cap/l2cap.h
+++
@@ -1,9 +0,0 @@
-#ifndef _L2CAP_H
-#define _L2CAP_H
-
-int l2cap_dial(const char *address, unsigned int psm, int *out_s);
-int l2cap_write(int s, const char* message);
-int l2cap_read(int s, const char* out_message);
-int l2cap_close(int s);
-
-#endif
