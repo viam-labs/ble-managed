9a249dbbd993c7e38166ce93fabf93d67ea14164
Benji Rewis <benji.rewis@viam.com>
2024-07-22 14:06:57 -0400

    print first two received

--- grill_proxy/src/socks.rs
+++ grill_proxy/src/socks.rs
@@ -19,6 +19,7 @@ pub async fn start_proxy(stream: &mut l2cap::Stream) -> bluer::Result<()> {
 
     // TODO: Use _addr to multiplex.
     while let Ok((mut tcp_stream, _addr)) = listener.accept().await {
+        let mut num_received_msgs = 2;
         loop {
             // Wait for the stream to be readable.
             debug!("Waiting for TCP stream to be readable...");
@@ -50,10 +51,15 @@ pub async fn start_proxy(stream: &mut l2cap::Stream) -> bluer::Result<()> {
             let mtu_as_cap = stream.as_ref().recv_mtu()?;
             let mut message_buf = vec![0u8; mtu_as_cap as usize];
             let n = stream.read(&mut message_buf).await?;
+            num_received_msgs += 1;
 
             message_buf.truncate(n);
             let length = message_buf.len();
             debug!("Writing response to TCP stream: length of message was {length}...");
+            if num_received_msgs < 3 {
+                // Only print message context for first 2 messages.
+                debug!("Writing response to TCP stream: message was {message_buf:#?}...");
+            }
             tcp_stream.write_all(&message_buf).await?;
         }
     }
