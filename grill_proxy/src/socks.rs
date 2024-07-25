30bb1996cb327d4567e0e59c2265b21dd256a98b
Benji Rewis <benji.rewis@viam.com>
2024-07-24 21:54:49 -0400

    full piping

--- grill_proxy/src/socks.rs
+++ grill_proxy/src/socks.rs
@@ -1,7 +1,7 @@
 //! Defines SOCKS forwarding logic.
 
 use crate::central;
-use log::{debug, error, info};
+use log::{debug, error, info, trace};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
@@ -17,95 +17,89 @@ pub async fn start_proxy(device: bluer::Device, psm: u16) -> bluer::Result<()> {
     let listener = TcpListener::bind(bind_address.clone()).await?;
     info!("SOCKS proxy now listening on {bind_address}");
 
-    // Set flow control.
-    // stream.as_ref().set_flow_control(l2cap::FlowControl::Le)?;
-
     // TODO: Use _addr to multiplex.
-    while let Ok((mut tcp_stream, _addr)) = listener.accept().await {
-        let mut num_received_msgs = 0;
+    while let Ok((tcp_stream, _addr)) = listener.accept().await {
         let device_clone = device.clone();
 
+        // Spawn a coroutine to handle incoming connection; continue to listen for more.
         tokio::spawn(async move {
-            // Create new stream
-            let mut stream = match central::connect_l2cap(device_clone, psm).await {
+            let (mut tcp_stream_read, mut tcp_stream_write) = tokio::io::split(tcp_stream);
+
+            let l2cap_stream = match central::connect_l2cap(device_clone, psm).await {
                 Ok(stream) => stream,
                 Err(e) => {
                     error!("Error creating L2CAP stream: {e}");
                     return;
                 }
             };
-            loop {
-                // Wait for the stream to be readable.
-                debug!("Waiting for TCP stream to be readable...");
-                if let Err(e) = tcp_stream.readable().await {
-                    error!("Error waiting for tcp stream to be readable: {e}");
-                    break;
+            let mtu_as_cap = match l2cap_stream.as_ref().recv_mtu() {
+                Ok(recv_mtu) => recv_mtu as usize,
+                Err(e) => {
+                    error!("Error getting recv_mtu from L2CAP stream: {e}");
+                    return;
                 }
+            };
+            let (mut l2cap_stream_read, mut l2cap_stream_write) = tokio::io::split(l2cap_stream);
 
-                // Try to read data, this may still fail with `WouldBlock` if the readiness event is a
-                // false positive.
-                let mut message_buf = vec![0u8; 1024];
-                match tcp_stream.try_read(&mut message_buf) {
-                    Ok(n) => {
-                        message_buf.truncate(n);
-                        debug!("Incoming request from TCP stream: {message_buf:#?}");
-                    }
-                    Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                        debug!("Continuing due to possible block...");
-                        continue;
-                    }
-                    Err(e) => {
-                        error!("Error reading from TCP stream: {e}");
-                        break;
-                    }
-                }
-
-                // Note that write_all will automatically split the buffer into
-                // multiple writes of MTU size.
-                debug!("Forwarding request to L2CAP stream...");
-                if let Err(e) = stream.write_all(&message_buf).await {
-                    error!("Error writing to L2CAP stream: {e}");
-                    break;
-                }
-
-                let mtu_as_cap = match stream.as_ref().recv_mtu() {
-                    Ok(recv_mtu) => recv_mtu,
-                    Err(e) => {
-                        error!("Error getting recv_mtu from L2CAP stream: {e}");
-                        break;
-                    }
-                };
-
+            // Spawn a coroutine to read from L2CAP stream and write to TCP stream.
+            tokio::spawn(async move {
                 loop {
                     debug!("Reading response from L2CAP stream...");
-                    let mut message_buf = vec![0u8; mtu_as_cap as usize];
-                    let n = match stream.read(&mut message_buf).await {
-                        Ok(n) => n,
+                    let mut message_buf = vec![0u8; mtu_as_cap];
+                    let n = match l2cap_stream_read.read(&mut message_buf).await {
+                        Ok(n) if n > 0 => n,
+                        Ok(_) => {
+                            debug!("L2CAP stream closed");
+                            break;
+                        }
                         Err(e) => {
                             error!("Error reading from L2CAP stream: {e}");
                             break;
                         }
                     };
-                    num_received_msgs += 1;
 
                     message_buf.truncate(n);
                     let length = message_buf.len();
-                    debug!("Writing response to TCP stream: length of message was {length}...");
-                    if num_received_msgs < 3 {
-                        // Only print message context for first 2 messages.
-                        debug!("Writing response to TCP stream: message was {message_buf:#?}...");
-                    }
-                    if let Err(e) = tcp_stream.write_all(&message_buf).await {
+                    debug!("Writing response of length {length} to TCP stream...");
+                    trace!("Response message was {message_buf:#?}");
+
+                    if let Err(e) = tcp_stream_write.write_all(&message_buf).await {
                         error!("Error writing to TCP stream: {e}");
                         break;
                     }
+                }
+            });
 
-                    if n != mtu_as_cap as usize {
-                        // No more!
+            // Spawn a coroutine to read from TCP stream and write to L2CAP stream.
+            tokio::spawn(async move {
+                loop {
+                    debug!("Reading request from TCP stream...");
+                    let mut message_buf = vec![0u8; 1024]; // TODO(better cap here);
+                    let n = match tcp_stream_read.read(&mut message_buf).await {
+                        Ok(n) if n > 0 => n,
+                        Ok(_) => {
+                            debug!("TCP stream closed");
+                            break;
+                        }
+                        Err(e) => {
+                            error!("Error reading from TCP stream: {e}");
+                            break;
+                        }
+                    };
+
+                    message_buf.truncate(n);
+                    let length = message_buf.len();
+                    debug!("Writing request of length {length} to L2CAP stream...");
+                    trace!("Request message was {message_buf:#?}");
+
+                    // Note that write_all will automatically split the buffer into multiple writes
+                    // of MTU size.
+                    if let Err(e) = l2cap_stream_write.write_all(&message_buf).await {
+                        error!("Error writing to L2CAP stream: {e}");
                         break;
                     }
                 }
-            }
+            });
         });
     }
 
