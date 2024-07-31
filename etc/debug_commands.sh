3e12e1e682ed62eb1c64e046f51d58b1f4400d43
Benji Rewis <benji.rewis@viam.com>
2024-07-31 18:23:00 +0200

    remove extra files

--- etc/debug_commands.sh
+++ etc/debug_commands.sh
@@ -1,2 +1,3 @@
+# Commands to help debug l2cap issues.
 echo "file net/bluetooth/l2cap_core.c +pfl" > /sys/kernel/debug/dynamic_debug/control
 echo "file net/bluetooth/l2cap_sock.c +pfl" > /sys/kernel/debug/dynamic_debug/control
--- go_server/go.mod
+++
@@ -1,3 +0,0 @@
-module testserver
-
-go 1.21.11
