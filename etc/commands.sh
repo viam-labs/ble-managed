20415c098e10338c255ef4586eaf81d111cd905d
Benji Rewis <benji.rewis@viam.com>
2024-08-20 10:39:14 -0400

    remove old files

--- etc/commands.sh
+++ etc/commands.sh
@@ -1,2 +1,3 @@
+# Helpful commands for debugging; turns on kernel debug logs for l2cap_core and l2cap_sock.
 echo "file net/bluetooth/l2cap_core.c +pfl" > /sys/kernel/debug/dynamic_debug/control
 echo "file net/bluetooth/l2cap_sock.c +pfl" > /sys/kernel/debug/dynamic_debug/control
