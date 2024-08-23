72e0c4afa37e503b9e8b57d81ae64f79c52a99b9
Benji Rewis <benji.rewis@viam.com>
2024-08-23 15:13:06 -0400

    even better chunking who knows

--- grill_proxy/src/socks/chunker.rs
+++ grill_proxy/src/socks/chunker.rs
@@ -1,49 +1,48 @@
 //! Defines a chunker.
 
-use std::io::Cursor;
-
 use anyhow::{anyhow, Result};
 use async_channel::Receiver;
 use log::debug;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::AsyncWriteExt;
 
 /// A chunker to read chunks of bytes from an `async_channel::Receiver`.
 pub(crate) struct Chunker {
     reader: Receiver<Vec<u8>>,
-    cursor: Cursor<Vec<u8>>,
+    chunk: Vec<u8>,
 }
 
 impl Chunker {
     pub(crate) fn new(reader: Receiver<Vec<u8>>) -> Self {
-        let cursor = Cursor::new(Vec::new()); // lazy
-        Chunker { reader, cursor }
-    }
-
-    fn bytes_remaining_in_cursor(&self) -> usize {
-        self.cursor.get_ref().len() - self.cursor.position() as usize
+        Chunker {
+            reader,
+            chunk: Vec::new(),
+        }
     }
 
     pub(crate) async fn read(&mut self, n: usize) -> Result<Vec<u8>> {
-        let mut buffer = vec![0; n];
-
-        // While chunk cursor is empty or does not have enough bytes for read; grab new chunks.
-        while self.bytes_remaining_in_cursor() < n {
-            let chunk = match self.reader.recv().await {
-                Ok(chunk) => chunk,
+        // While chunk does not have enough bytes for read; grab new chunks.
+        while self.chunk.len() < n {
+            let new_chunk = match self.reader.recv().await {
+                Ok(new_chunk) => new_chunk,
                 Err(e) => {
                     return Err(anyhow!("could not get new chunk: {e}"));
                 }
             };
+            //
             // TODO(benji): remove this debug
             debug!("Got a new chunk!");
-            if let Err(e) = self.cursor.write(&chunk).await {
-                return Err(anyhow!("could not write new chunk: {e}"));
+
+            match self.chunk.write(&new_chunk).await {
+                Ok(n) if n > 0 => {}
+                Ok(_) => {
+                    return Err(anyhow!("overflowed writing to chunk"));
+                }
+                Err(e) => {
+                    return Err(anyhow!("could not write new chunk: {e}"));
+                }
             }
         }
 
-        if let Err(e) = self.cursor.read_exact(&mut buffer).await {
-            return Err(anyhow!("could not read {n} bytes from current chunk: {e}"));
-        }
-        Ok(buffer)
+        Ok(self.chunk.drain(0..n).collect())
     }
 }
