5ebf8929818bc96c5dafe22b1ddb944257e22a03
Alden Hsu <alden.hsu@viam.com>
2025-10-10 09:41:58 -0400

    set -e for bluez install

--- etc/install_bluez.sh
+++ etc/install_bluez.sh
@@ -41,6 +41,8 @@ sudo apt install python3-docutils
 echo "Installing git (likely already installed)..."
 sudo apt install git
 
+set -e
+
 echo "Cloning bluez..."
 pushd ~
 git clone https://github.com/bluez/bluez.git
