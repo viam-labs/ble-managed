4f2af6bd1676c0c36f57c4b46f5fe1cab43a21d5
aldenh-viam <alden.hsu@viam.com>
2025-10-16 10:09:47 -0400

    apt install libbluetooth-dev (#22)
    * apt install libbluetooth-dev
    
    * echo libbluetooth-dev install
    
    Co-authored-by: Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>

--- etc/install_bluez.sh
+++ etc/install_bluez.sh
@@ -40,6 +40,8 @@ sudo apt install python3-docutils
 
 echo "Installing git (likely already installed)..."
 sudo apt install git
+echo "Installing libbluetooth-dev..."
+sudo apt install libbluetooth-dev
 
 echo "Cloning bluez..."
 pushd ~
