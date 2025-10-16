e2b43955955f622d910e9159cdc101d77d98d1c6
aldenh-viam <alden.hsu@viam.com>
2025-10-16 10:09:17 -0400

    RSDK-12035: Restart manageMachine if connection establishment does not happen after 45s (#21)
    * retry manageMachine if connection establishment does not happen after 45s
    
    * await disconnect to catch exception
    
    Co-authored-by: Kevin Johnson <kevinjohnsonutx@gmail.com>
    
    * Revert "await disconnect to catch exception"
    
    This reverts commit 4405aad7eb40a0779159f7d62c8c594c88b4b5e0.
    
    ---------
    
    Co-authored-by: Kevin Johnson <kevinjohnsonutx@gmail.com>

--- phone_proxy/lib/main.dart
+++ phone_proxy/lib/main.dart
@@ -103,6 +103,7 @@ void mainLoop(String mobileDevice, machineToManage, bool shouldCallRunApp) {
         // grill.
 
         logger.w('disconnection detected; restarting pairing process');
+        bridgeEstablished = false;
         // Restart zone but don't call runApp to avoid zone mismatch.
         mainLoop(mobileDevice, machineToManage, false);
       } else {
@@ -124,6 +125,8 @@ const viamSocksProxyNameCharUUID = '918ce61c-199f-419e-b6d5-59883a0049d8';
 
 // Give some BLE operations a few retries for resiliency.
 const numRetries = 3;
+
+var bridgeEstablished = false;
 
 void startBLESocksPhoneProxy(String mobileDevice, machineToManage) {
   WidgetsFlutterBinding.ensureInitialized();
@@ -177,6 +180,7 @@ Future<void> listenAndProxySOCKS(Stream<L2CapChannel> chanStream) async {
   logger.i('in healthy and idle state; scanning for devices to proxy traffic from');
 
   chanStream.listen((chan) async {
+    bridgeEstablished = true;
     final thisCount = chanCount++;
     logger.i('BLE-SOCKS bridge established and ready to handle traffic');
     final socksServerProxy = SocksServer();
@@ -274,6 +278,22 @@ Future<void> manageMachine(BleCentral bleCentral, String mobileDevice, machineTo
         }
 
         logger.i('machine to manage knows our name and we will wait for a connection');
+
+        // At this point, if successfully paired, connection establishment should happen immediately after.
+        // Retry if this is not the case 45 seconds from this point.
+        // Primarily to fix that the pairing prompt times out after 30s and does not reappear,
+        // so we do not want to be waiting indefinitely if the user misses it or accidentally cancels.
+        await Future.doWhile(() => Future.delayed(const Duration(seconds: 1))
+                .then((_) => !bridgeEstablished))
+            .timeout(const Duration(seconds: 45), onTimeout: () {
+          try {
+            logger.i('connection establishment timeout, disconnecting periph before retrying...');
+            periph.disconnect();
+          } catch (error) {
+            logger.w('failed to disconnect $periph before retrying: $error');
+          }
+          throw TimeoutException("Connection establishment timeout");
+        });
       }).catchError((error) {
         logger.e('error establishing connection with machine to manage: $error; will try again');
         unawaited(manageMachine(bleCentral, mobileDevice, machineToManage));
